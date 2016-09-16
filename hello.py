1f6017847278e8ee383ed9853871853d82166525
Eirin <ghkdgustn29@gmail.com>
2016-09-16 18:32:04 +0900

    html, css, javascript 예시

--- hello.py
+++ hello.py
@@ -3,7 +3,30 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, Hello, Hello!'
+    return """
+<!doctype html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <style>
+  body { text-align: center; }
+
+  p { color: gray; }
+  </style>
+</head>
+<body>
+  <h1 onclick="hi()">Hello, world!</h1>
+  <hr>
+  <p>안녕!</p>
+
+<script>
+function hi() {
+  document.querySelector('body > p').innerHTML += " 안녕!";
+}
+</script>
+</body>
+</html>
+"""
 
 @app.route('/bone')
 def hello_bone():
