a4f47666a206af9ff46b88046ebb8697207df472
Eirin <ghkdgustn29@gmail.com>
2016-09-16 18:27:01 +0900

    add 'name' route
    라우트 추가.

--- hello.py
+++ hello.py
@@ -9,5 +9,9 @@ def hello_world():
 def hello_bone():
     return 'idiot!'
 
+@app.route('/bone/<name>')
+def hello_name(name):
+    return name
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
