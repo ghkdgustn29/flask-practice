3db76cc02f90db0bad80d2587b8c19b9011bec55
Eirin <ghkdgustn29@gmail.com>
2016-09-16 18:18:38 +0900

    Add '/bone' route
    새 경로 추가

--- hello.py
+++ hello.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, Hello, Hello!'
 
+@app.route('/bone')
+def hello_bone():
+    return 'idiot!'
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
